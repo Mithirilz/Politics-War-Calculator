5a688f716706f0f0b0a5ec770629b7448a993e9f
Mithirilz <phalanxcortex@gmail.com>
2026-07-06 23:15:49 +0800

    Ensured typehinting was written for params

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ def main():
     print("test run")
     return
 
-def calc_turns_for_goal_treasury(desired_treasury: int, current_treasury):
+def calc_turns_for_goal_treasury(desired_treasury: int, current_treasury: int):
+
     return
